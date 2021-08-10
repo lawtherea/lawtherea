da8677644d1b9dd9a9a2e21aab8fcc1ad36fe1c8
Leila Biggi <87096464+lawtherea@users.noreply.github.com>
2021-08-10 20:52:28 -0300

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+adj = input("Adjective: ")
+place = input("Place: ")
+color = input("Color: ")
+subs = input("Substantive: ")
+eat1 = input("Edible 1: ")
+eat2 = input("Edible 2: ")
+
+madlib = f"It's been a {adj} winter here in {place}. \
+For as long as we can remember, the sky has been {color} and the ground has been covered with {subs}. \
+We have all been surviving on a steady diet of {eat1} and {eat2}."
+
+print(madlib)
